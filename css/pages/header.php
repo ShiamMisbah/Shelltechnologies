d6610cbb7671cd210ff0de4a26fa6c1d90793694
ShiamMisbah <74020444+ShiamMisbah@users.noreply.github.com>
2022-05-18 14:29:16 +0600

    Add files via upload

---
+++ css/pages/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/c8a343a876.js" crossorigin="anonymous"></script>
+     <link rel="stylesheet" href="css/style.css">
+     <!-- owl carousel -->
+     <link rel = "stylesheet" href = "owl_carousel/owl.carousel.css">
+     <link rel = "stylesheet" href = "owl_carousel/owl.theme.default.css">
+     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  
+    
+    <title>SHELL TECHNOLOGIES</title>
+</head>
+<body>
+    <!-------------Top Bar------------------->
+    <section class="bg-dark p-2 top-bar" >
+      <div class="container" >
+        <div class="row" style="max-height: 100px;">
+          <div class="col">
+            <span class="text-white">
+              <h4 style="font-size: 13PX; padding: 5px; margin: 0px;">SOLUTION, THAT YOU CAN RELY UPON</h4>
+            </span>
+          </div>
+          <div class="col text-right">
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <i class="fa-solid fa-phone"></i>
+              +8801882581470
+
+            </span>
+
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <i class="fa-solid fa-envelope"></i>
+              Webmail
+
+            </span>
+
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <i class="fa-solid fa-headset"></i>
+              LiveChat
+
+            </span>
+
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <i class="fa-solid fa-phone-rotary"></i>
+              Support
+
+            </span>
+
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <a href="" style="color: white;" class="top-btn">DASHBOARD LOGIN</a>
+
+            </span>
+
+            <span class="text-white" style="font-size: 12px; padding: 5px;">
+              <i class="fa-solid fa-cart-arrow-down"></i>
+              0
+
+            </span>
+
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+    <!-------------End Top Bar------------------->
+    
+    <!--------------Nav---------------------->
+    <nav>
+      <div class="logo">
+         <a href="index.html" class="nav-text">SHELL <SPan style="color: black;">TECHNOLOGIES</SPan></a>
+      </div>
+      <input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
+      <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+         
+         <li><a href="index.html">HOME</a></li>
+         <li><a href="#">ABOUT</a></li>
+         <li><a href="#">OUR WORKS</a></li>
+         <li><a href="#">HOSTING</a></li>
+         <li><a href="#">DOMAIN</a></li>
+         <li><a href="#">SUPPORT</a></li>
+         <li><a href="#">CONTACT US</a></li>
+      </ul>
+   </nav>
+
+    <!---------------End Nav-------------------->
